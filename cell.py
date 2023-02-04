714597c4b21774e07209183a9348643537429b3c
Derya Gumustel <yoshdragon@gmail.com>
2023-02-04 09:49:45 -0800

    Add display showing # cells remaining

--- cell.py
+++ cell.py
@@ -1,12 +1,14 @@
-from tkinter import Button
+from tkinter import Button, Label
 import random
 import settings
 
 class Cell:
     all = []
-
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_opened = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -25,10 +27,27 @@ class Cell:
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
 
+    @staticmethod # Use case of Class, not instance
+    def create_cell_count_label(location):
+        lbl = Label(
+            location, 
+            text=f'Cells left: {Cell.cell_count}',
+            width=12,
+            height=4,
+            bg='black',
+            fg='white',
+            font=('', 30)
+        )
+        Cell.cell_count_label_object = lbl
+
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
+            # If mine count is 0, expand view to all surrounding cells
+            if self.surrounded_cells_mines_length == 0:
+                for cell_object in self.surrounding_cells:
+                    cell_object.show_cell()
             self.show_cell()
 
     def get_cell_by_axis(self, x, y):
@@ -61,7 +80,16 @@ class Cell:
         return counter
 
     def show_cell(self):
-        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            # Recalculate cell count label text 
+            self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f'Cells left: {Cell.cell_count}'
+                )
+            
+        self.is_opened = True
 
     def show_mine(self):
         # TODO: Program game over
--- main.py
+++ main.py
@@ -48,6 +48,10 @@ for x in range(settings.GRID_WIDTH):
             column=x, row=y
         )
 
+# Call label from Cell class
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(x=0, y=0)
+
 Cell.randomize_mines()
 
 for c in Cell.all:
